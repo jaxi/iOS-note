cfaedb800081389c4735b0381b301e0cd001a3d5
Jingkai He <jaxihe@gmail.com>
2014-06-23 18:07:45 +0100

    Initial Commit

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by Jingkai He on 23/06/2014.
+//  Copyright (c) 2014 Jingkai He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Notes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Notes
+//
+//  Created by Jingkai He on 23/06/2014.
+//  Copyright (c) 2014 Jingkai He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
